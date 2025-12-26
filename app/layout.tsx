aa7d0a4f2ce27a52b4b3dc28829fc1d9774d6bf9
TOMOCLOUD-DEV <info@tomocloud.co.jp>
2025-12-26 11:38:57 +0800

    add: ニュース

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 // app/layout.tsx
 import './globals.css';
 import type { ReactNode } from 'react';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
   title: '株式会社TOMOCLOUD ～新しい自分が視える世界へ～',
   description: '視える化技術により全ての人が自分らしく生きることのできる社会を創造する',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
